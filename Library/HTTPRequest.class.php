efa8dabfb1cefcaeb388b835f8741a0811baf160
collettemathieu <collettemathieu@noolib.com>
2017-02-10 10:08:47 +0100

    Correction bug HTTPRequest

--- Library/HTTPRequest.class.php
+++ Library/HTTPRequest.class.php
@@ -31,10 +31,8 @@ class HTTPRequest extends \Library\ApplicationComponent
 					'#\(#', 
 					'#\)#', 
 					'#\'#', 
-					'#"#', 
-					'#\}#', 
+					'#"#',  
 					'#\]#', 
-					'#\{#', 
 					'#\[#'
 				);
 
