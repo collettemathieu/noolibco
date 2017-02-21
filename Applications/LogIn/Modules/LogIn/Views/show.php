9465c79f487e3660ef38fadf9fad96fcf52f0ccc
collettemathieu <collettemathieu@noolib.com>
2017-02-21 10:57:17 +0100

    Css Login z-Index

--- Applications/LogIn/Modules/LogIn/Views/show.php
+++ Applications/LogIn/Modules/LogIn/Views/show.php
@@ -4,7 +4,7 @@
 				<div class="col-sm-6">
 					<!-- Si JS n'est pas activé -->
 			        <noscript>
-			            <div class="col-lg-12 alertJS alert alert-warning">
+			            <div class="col-lg-6 alertJS alert alert-warning">
 			            	<span>JavaScript is not enabled.</span>
 			            	<span>NooLib does not work properly without JavaScript enabled <a href="https://support.google.com/adsense/answer/12654?hl=fr" target="_blank">(Help ?)</a>.</span>
 			            </div>
