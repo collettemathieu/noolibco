e4c0e72cb5007fcfe2b9f7fdcaa68f4c8db2a0d0
collettemathieu <collettemathieu@noolib.com>
2017-02-22 18:46:30 +0100

    Angular Add A Team In Profile

--- public_html/JavaScript/ApplicationsJS/ProfileUser/Controllers/profileUserController.js
+++ public_html/JavaScript/ApplicationsJS/ProfileUser/Controllers/profileUserController.js
@@ -16,6 +16,6 @@
 
 application.controller('profileUserController', ['teamService', '$scope', '$http', function(teamService, $scope, $http){
 	var institutions = teamService.getInstitutions();
-	console.log(institutions[1].name);
+	console.log(institutions);
 }]);
 
