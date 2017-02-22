b3d3ed08777e69a5e094a2a5044e26f7523ef735
collettemathieu <collettemathieu@noolib.com>
2017-02-22 15:59:41 +0100

    Angular Add A Team In Profile

---
+++ public_html/JavaScript/ApplicationsJS/ProfileUser/Services/teamService.js
@@ -0,0 +1,65 @@
+// +----------------------------------------------------------------------+
+// | AngularJS Version 1.5.9						                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2017 NooLib			         				          |
+// +----------------------------------------------------------------------+
+// | Service pour récupérer des informations d'une institution, 		  |
+// | d'un laboratoire ou d'une équipe.									  |
+// +----------------------------------------------------------------------+
+// | Auteur : Mathieu COLLETTE <collettemathieu@noolib.com>    			  |
+// +----------------------------------------------------------------------+
+
+/**
+ * @name:  Service teamService
+ * @access: public
+ * @version: 1
+ */
+
+application.factory('teamService', ['$q', '$http', function($q, $http){
+	return{
+		getInstitutions: function(){
+			// On importe les publications déjà enregistrées de l'application
+			var deferred = $q.defer();
+			$http({
+				method: 'POST',
+				url: '/Profile/GetInstitutions'
+			})
+			.success(function(response){
+				deferred.resolve(response);
+			})
+			.error(function(error){
+				displayInformationsClient(error);
+			});
+
+			return deferred.promise;
+		},
+		deletePublication: function(idPublication, idApplication){
+			// On importe les publications déjà enregistrées de l'application
+			var deferred = $q.defer();
+			$http({
+				method: 'POST',
+				url: '/HandleApplication/DeletePublication',
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+				transformRequest: function(obj) {
+			        var str = [];
+			        for(var p in obj)
+			        	str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+			        return str.join("&");
+			    },
+				data: {
+					idApp: idApplication,
+					idPublication: idPublication
+				}
+			})
+			.success(function(response){
+				deferred.resolve(response);
+			})
+			.error(function(error){
+				displayInformationsClient(error);
+			});
+
+			return deferred.promise;
+		}
+	};
+}]);
+
