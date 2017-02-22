1be3efed5548a734f7c14ebe4de52938f3be5d3e
collettemathieu <collettemathieu@noolib.com>
2017-02-22 15:48:25 +0100

    Angular Add A Team In Profile

--- Applications/Profile/Libraries/JavaScript/load.php
+++ Applications/Profile/Libraries/JavaScript/load.php
@@ -1,5 +1,5 @@
 <!-- Files JS -->
 <script type="text/javascript" src="/JavaScript/Frameworks/angular.min.js"></script>
 <script type="text/javascript" src="/JavaScript/ApplicationsJS/ProfileUser/applicationRun.js"></script>
-<script type="text/javascript" src="/JavaScript/ApplicationsJS/ProfileUser/Controllers/profileUserController.js"></script>
 <script type="text/javascript" src="/JavaScript/ApplicationsJS/ProfileUser/Services/teamService.js"></script>
+<script type="text/javascript" src="/JavaScript/ApplicationsJS/ProfileUser/Controllers/profileUserController.js"></script>
