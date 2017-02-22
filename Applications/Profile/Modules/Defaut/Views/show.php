e2ffc2078ccd00e45667ff769fc527691b1e456c
collettemathieu <collettemathieu@noolib.com>
2017-02-22 19:56:14 +0100

    Angular Add A Team In Profile

--- Applications/Profile/Modules/Defaut/Views/show.php
+++ Applications/Profile/Modules/Defaut/Views/show.php
@@ -281,7 +281,7 @@
 								</table>
 							</div>
 							<div>
-								<table class="table table-bordered table-striped table-condensed" style="vertical-align:middle;">
+								<table class="table table-bordered table-striped table-condensed">
 									<caption>Add yourself to a team</caption>
 									<tr>
 										<td>Institution:</td>
