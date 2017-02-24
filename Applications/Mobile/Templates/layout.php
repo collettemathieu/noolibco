405db673385d64ddc13cd87ce7611bc43fc2ed05
collettemathieu <collettemathieu@noolib.com>
2017-02-24 16:50:05 +0100

    Template Mobile

--- Applications/Mobile/Templates/layout.php
+++ Applications/Mobile/Templates/layout.php
@@ -31,3 +31,84 @@ Go to http://www.noolib.com/Hire/ .
 
 	<body><?php echo $content; ?></body>
 </html>
+
+<!DOCTYPE HTML>
+<html dir="ltr" lang="fr-FR">
+<!--
+
+ _   _             _      _ _     
+| \ | |           | |    (_) |    
+|  \| | ___   ___ | |     _| |__  
+| . ` |/ _ \ / _ \| |    | | '_ \ 
+| |\  | (_) | (_) | |____| | |_) |
+|_| \_|\___/ \___/|______|_|_.__/  Hire !
+
+Go to https://www.noolib.com/Hire/ .
+
+-->
+
+	<head>
+		
+		<meta charset="UTF-8">
+
+		<!-- Chargement des librairies CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="/Css/styleMobile.css" />
+
+		<!-- Insertion d'une icône dans la barre latérale du navigateur -->
+		<link rel="apple-touch-icon" sizes="57x57" href="/Images/Favicon/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="/Images/Favicon/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/Images/Favicon/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="/Images/Favicon/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/Images/Favicon/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/Images/Favicon/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/Images/Favicon/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/Images/Favicon/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/Images/Favicon/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="/Images/Favicon/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/Images/Favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="/Images/Favicon/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/Images/Favicon/favicon-16x16.png">
+		<link rel="manifest" href="/Images/Favicon/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="/Images/Favicon/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+		
+		<meta name="keywords" content="Library, application, research, plateform, internet, scientific, program, informatic, c, c++, python, java, javascript, php, submit, promote, simulator, education, researches, researcher, phd, student, engineer, university, school, professor"/>
+		
+		<meta name="description" content="Create and promote your scientific applications for your researches."/>
+		<title>Welcome - NooLib Web Application</title>
+
+	</head>
+
+	<body>
+		<!-- LOADER -->
+		<?php if($_SERVER['REQUEST_URI'] != '/LogIn/JSNotActivated'){?>
+		<div class="overlay">
+			<div class="globalLoader"></div>
+		</div>
+		<?php }?>
+		<script>
+		window.onload=function(){
+			$('.overlay').fadeOut();
+		};
+		</script>
+
+		
+		<!-- SECTION -->
+		<section>
+		<?php
+			echo $content;
+		?>
+		</section>
+		
+		<!-- FOOTER -->
+		<?php
+			include_once('../public_html/footer.php');
+		?>
+
+		<!-- Chargement des librairies JS -->
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+	</body>
+</html>
