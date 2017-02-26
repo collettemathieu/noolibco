7988d7338e81a993fc34ee8c87d99036b4287c79
collettemathieu <collettemathieu@noolib.com>
2017-02-26 12:15:00 +0100

    Modify Template Mobile

--- Applications/Mobile/Libraries/CSS/load.php
+++ Applications/Mobile/Libraries/CSS/load.php
@@ -1,6 +1,5 @@
 <!-- FrameWorks CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
-<link rel="stylesheet" href="/Css/Frameworks/styleBootstrapSwitch.min.css">
 
 <!-- Files CSS -->
 <link rel="stylesheet" href="/Css/styleMobile.css" />
