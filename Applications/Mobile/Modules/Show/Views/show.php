148c630424f3316e578166de3e1c0433be6b438b
collettemathieu <collettemathieu@noolib.com>
2017-02-26 12:30:36 +0100

    Modify Template Mobile

--- Applications/Mobile/Modules/Show/Views/show.php
+++ Applications/Mobile/Modules/Show/Views/show.php
@@ -1,16 +1,18 @@
 	<div class="container-fluid">
 		<div class="row-fluid fadeIn">
-			<!-- Info Mobile -->
-		    <div class="col-lg-6 centering text-center alert alert-warning">
-	        	<span>Use NooLib on your mobile.</span>
-	        	<span>Please stay tune !</span>
-		    </div><br>
-			<div class="col-lg-6 centering">
-				<h1>Welcome to NooLib.</h1>
-				<h2>NooLib offers a library of scientific applications for Research.</h2>
-				<h2>It is very simple to submit your own algorithms written in C/C++/JS/Java/PHP or Python.</h2>
-				<h2>Interact smartly with others applications and share your results...</h2>
-				<button class="btn btn-default" ng-click="aboutModal()">About !</button>
+			<div class="col-lg-12 centering">
+				<!-- Info Mobile -->
+			    <div class="col-lg-6 text-center alert alert-warning">
+		        	<span>Use NooLib on your mobile.</span>
+		        	<span>Please stay tune !</span>
+			    </div><br>
+				<div class="col-lg-6">
+					<h1>Welcome to NooLib.</h1>
+					<h2>NooLib offers a library of scientific applications for Research.</h2>
+					<h2>It is very simple to submit your own algorithms written in C/C++/JS/Java/PHP or Python.</h2>
+					<h2>Interact smartly with others applications and share your results...</h2>
+					<button class="btn btn-default" ng-click="aboutModal()">About !</button>
+				</div>
 			</div>
 		</div>
 	</div>
