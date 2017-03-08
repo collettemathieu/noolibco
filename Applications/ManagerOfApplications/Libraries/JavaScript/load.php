d8f7948dd869d652d70aa665e7a7d8e7f74a60d8
collettemathieu <collettemathieu@noolib.com>
2017-03-08 10:41:14 +0100

    Style Applications Manager

--- Applications/ManagerOfApplications/Libraries/JavaScript/load.php
+++ Applications/ManagerOfApplications/Libraries/JavaScript/load.php
@@ -5,3 +5,15 @@
 <!-- Others libraries JS -->
 <script type="text/javascript" src="/JavaScript/arbreApplication.js"></script>
 <script type="text/javascript" src="/JavaScript/fonctionsForApplication.js"></script>
+
+<!-- Files JS -->
+<script type="text/javascript" src="/JavaScript/Frameworks/angular.min.js"></script>
+<script type="text/javascript" src="/JavaScript/Frameworks/angular-animate.min.js"></script>
+<script type="text/javascript" src="/JavaScript/Frameworks/ui-bootstrap-tpls-1.2.5.min.js"></script>
+<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/applicationRun.js"></script>
+<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/Controllers/logInController.js"></script>
+<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/Controllers/aboutController.js"></script>
+<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/Controllers/contactController.js"></script>
+<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/Controllers/registrationController.js"></script>
+<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/Directives/switchDirective.js"></script>
+<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/Directives/passwordValidatorDirective.js"></script>
---
+++ public_html/JavaScript/ApplicationsJS/ApplicationsManager/applicationRun.js
@@ -0,0 +1,26 @@
+// +----------------------------------------------------------------------+
+// | AngularJS Version 1.5.9						                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2017 NooLib			         				          |
+// +----------------------------------------------------------------------+
+// | Application ApplicationsManager									  |
+// +----------------------------------------------------------------------+
+// | Auteur : Mathieu COLLETTE <collettemathieu@noolib.com>    			  |
+// +----------------------------------------------------------------------+
+
+/**
+ * @name:  Application ApplicationsManager
+ * @access: public
+ * @version: 1
+ */
+
+var application = angular.module('applicationApplicationsManager', ['ngAnimate', 'ui.bootstrap']);
+
+/* Configuration */
+application.config(['$locationProvider', '$httpProvider', '$compileProvider', function($locationProvider, $httpProvider, $compileProvider){
+	$locationProvider.html5Mode(true); // Activation du mode HTML 5 pour l'url des routes
+	$httpProvider.defaults.cache = true; // Activation du cache pour toutes les requêtes HTTP de type GET
+	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'; // Activation du header Ajax
+	$compileProvider.debugInfoEnabled(false); // Désactivation des informations de debug afin de gagner en performance
+
+}]);
