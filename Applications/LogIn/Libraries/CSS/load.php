4091cdce0678d8b06bd6e9c58545f6eead2c2ecc
collettemathieu <collettemathieu@noolib.com>
2017-03-08 18:46:39 +0100

    Boostrap Theme CSS

--- Applications/LogIn/Libraries/CSS/load.php
+++ Applications/LogIn/Libraries/CSS/load.php
@@ -1,6 +1,7 @@
 <!-- FrameWorks CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
-<link rel="stylesheet" href="/Css/Frameworks/styleBootstrapSwitch.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" crossorigin="anonymous"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<link rel="stylesheet" href="/Css/Frameworks/styleBootstrapSwitch.min.css" crossorigin="anonymous">
 
 <!-- Files CSS -->
 <link rel="stylesheet" href="/Css/styleLogIn.css" />
--- Applications/Mobile/Libraries/CSS/load.php
+++ Applications/Mobile/Libraries/CSS/load.php
@@ -1,5 +1,6 @@
 <!-- FrameWorks CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" crossorigin="anonymous"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 <!-- Files CSS -->
 <link rel="stylesheet" href="/Css/styleMobile.css" />
