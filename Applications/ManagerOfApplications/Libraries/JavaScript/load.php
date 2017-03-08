d7d0635b3cb70f62eb8a491f22cd443e8d9b5d07
collettemathieu <collettemathieu@noolib.com>
2017-03-08 10:43:54 +0100

    Create JS ApplicationsManager

--- Applications/ManagerOfApplications/Libraries/JavaScript/load.php
+++ Applications/ManagerOfApplications/Libraries/JavaScript/load.php
@@ -10,10 +10,4 @@
 <script type="text/javascript" src="/JavaScript/Frameworks/angular.min.js"></script>
 <script type="text/javascript" src="/JavaScript/Frameworks/angular-animate.min.js"></script>
 <script type="text/javascript" src="/JavaScript/Frameworks/ui-bootstrap-tpls-1.2.5.min.js"></script>
-<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/applicationRun.js"></script>
-<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/Controllers/logInController.js"></script>
-<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/Controllers/aboutController.js"></script>
-<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/Controllers/contactController.js"></script>
-<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/Controllers/registrationController.js"></script>
-<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/Directives/switchDirective.js"></script>
-<script type="text/javascript" src="/JavaScript/ApplicationsJS/LogIn/Directives/passwordValidatorDirective.js"></script>
+<script type="text/javascript" src="/JavaScript/ApplicationsJS/ApplicationsManager/applicationRun.js"></script>
