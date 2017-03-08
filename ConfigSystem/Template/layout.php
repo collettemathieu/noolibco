15bdafa7651fea8c5cd8823366ea0af381209d63
collettemathieu <collettemathieu@noolib.com>
2017-03-08 18:40:39 +0100

    Test CSS boostrap

--- ConfigSystem/Template/layout.php
+++ ConfigSystem/Template/layout.php
@@ -21,7 +21,6 @@ Go to https://www.noolib.com/Hire/ .
 		<link rel="stylesheet" href="/Css/Frameworks/styleBreadCrumb.min.css" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
 		<link rel="stylesheet" href="https://code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css"/>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 		
 		<!-- Files CSS -->
 		<link rel="stylesheet" href="/Css/styleGeneral.css" />
