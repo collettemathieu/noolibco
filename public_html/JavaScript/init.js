f8cf9b429f1b00b24b6c0ad9c0e55676274ad4e3
collettemathieu <collettemathieu@noolib.com>
2017-03-09 14:59:00 +0100

    Style des input avec JS

--- public_html/JavaScript/init.js
+++ public_html/JavaScript/init.js
@@ -43,7 +43,7 @@
 
 	// Pour modifier le design des inputs file
 	$('[type="file"]').filestyle({
-		buttonName: "btn-primary"
+		iconName: 'glyphicon-download-alt'
 	});
 
 	// Pour afficher l'aide à l'utilisateur
