31d79051a5a036e91b925aa7f75b953f19185399
collettemathieu <collettemathieu@noolib.com>
2017-03-09 16:41:48 +0100

    Select Directive Design LogIn

--- Applications/LogIn/Libraries/JavaScript/load.php
+++ Applications/LogIn/Libraries/JavaScript/load.php
@@ -1,5 +1,6 @@
 <!-- FrameWorks and Libraries JS -->	
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="/JavaScript/Frameworks/bootstrapForLogInApplication.min.js"></script>
 <script type="text/javascript" src="/JavaScript/Frameworks/bootstrapSwitch.min.js"></script>
 
 <!-- Critical scripts -->
