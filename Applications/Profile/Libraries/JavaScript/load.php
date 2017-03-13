0e97807d15cd42465137590fef0cf1b618d1e373
collettemathieu <collettemathieu@noolib.com>
2017-03-13 18:40:38 +0100

    Bug Profile Angular

--- Applications/Profile/Libraries/JavaScript/load.php
+++ Applications/Profile/Libraries/JavaScript/load.php
@@ -4,5 +4,6 @@
 <!-- Files JS -->
 <script type="text/javascript" src="/JavaScript/ApplicationsJS/ProfileUser/applicationRun.js"></script>
 <script type="text/javascript" src="/JavaScript/ApplicationsJS/ProfileUser/Services/teamService.js"></script>
+<script type="text/javascript" src="/JavaScript/ApplicationsJS/ProfileUser/Directives/selectDesignDirective.js"></script>
 <script type="text/javascript" src="/JavaScript/ApplicationsJS/ProfileUser/Controllers/profileUserController.js"></script>
 
