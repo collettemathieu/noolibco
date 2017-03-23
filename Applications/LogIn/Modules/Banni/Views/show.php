9071bcc8a1cee52139e7732b61ee4821affe85c7
noolibco <noolibco@DebianNooLib>
2017-03-23 20:11:54 +0100

    Correction page Banni LogIn

--- Applications/LogIn/Modules/Banni/Views/show.php
+++ Applications/LogIn/Modules/Banni/Views/show.php
@@ -4,7 +4,7 @@
 			<img src="/Images/banned.png"/>
 			<div class="jumbotron">
 				<p>You have been banished by the NooLib team!</p>
-				<p>For more information, please <a data-toggle="modal" href="#contact">contact us</a>.</p>
+				<p>For more information, please <a ng-click="contactModal()">contact us</a>.</p>
 			</div>
 		</div>
 	</div>
