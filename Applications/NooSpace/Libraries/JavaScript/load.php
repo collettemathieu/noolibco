1418ec436453be238db07394859cc324fcfa1324
noolibco <noolibco@debianNoolib>
2017-04-03 11:10:55 +0200

    modifié :         Applications/NooSpace/Libraries/JavaScript/load.php

--- Applications/NooSpace/Libraries/JavaScript/load.php
+++ Applications/NooSpace/Libraries/JavaScript/load.php
@@ -7,11 +7,11 @@
 
 
 <!-- Others libraries JS -->
-<!--script type="text/javascript" src="/JavaScript/nooSpace.js"></script>
+<script type="text/javascript" src="/JavaScript/nooSpace.js"></script>
 <script type="text/javascript" src="/JavaScript/fonctionsForApplication.js"></script>
 <script type="text/javascript" src="/JavaScript/fonctionsForData.js"></script>
 <script type="text/javascript" src="/JavaScript/addData.js"></script>
-<script type="text/javascript" src="/JavaScript/showData.js"></script-->
+<script type="text/javascript" src="/JavaScript/showData.js"></script>
 
 
 <!-- Files JS -->
