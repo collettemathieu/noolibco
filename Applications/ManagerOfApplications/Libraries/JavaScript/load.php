0718e47c39df3e77b72cc06773b0fd302fa6363a
noolibco <noolibco@DebianNooLib>
2017-04-17 19:53:26 +0200

    Merge ApplicationsManager erreur delete File JS

--- Applications/ManagerOfApplications/Libraries/JavaScript/load.php
+++ Applications/ManagerOfApplications/Libraries/JavaScript/load.php
@@ -4,9 +4,6 @@
 <script type="text/javascript" src="/JavaScript/Frameworks/loDash.min.js"></script>
 <script type="text/javascript" src="/JavaScript/Frameworks/dropzone.min.js"></script>
 <script type="text/javascript" src="/JavaScript/Frameworks/progressBar.min.js"></script>
-<!-- Others libraries JS -->
-<script type="text/javascript" src="/JavaScript/arbreApplication.js"></script>
-<script type="text/javascript" src="/JavaScript/fonctionsForApplication.js"></script>
 
 <!-- Files JS -->
 <script type="text/javascript" src="/JavaScript/Frameworks/angular.min.js"></script>
