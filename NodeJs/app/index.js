c195cac0ab41ff17523e3b2a5beda83f98e6b075
root <root@debianNoolib>
2017-06-23 17:29:12 +0200

    NodeJS Serveur problème Script Bash

--- NodeJs/app/index.js
+++ NodeJs/app/index.js
@@ -24,6 +24,7 @@ var InputDonneeUtilisateur= require('./InputDonneeUtilisateur');
 var Fonction=require('../models/Fonction');
 var MessageClient = require('../models/MessageClient');
 var messageClient = new MessageClient();
+var Config=require('../models/Config');
 //variable de stockage
 var abonnement_user=true;
 var outputData;
@@ -87,33 +88,46 @@ function executeRun(fields,currentUtilisateur, applicationRunning,numVersionRunn
 				if(tacheDatas.length === tabDonneeUtilisateur.length){
 				var i=-1;
 				
-				tacheDatas.forEach(function(tacheData){
+				tacheDatas.forEach( async function(tacheData){
 					i++;
 					var donnee = tabDonneeUtilisateur[i];
 					var typeDonnee = donnee.getTypeDonneeUtilisateur().getExtensionTypeDonneeUtilisateur();
 					var typeAttendu = tacheData.getTypeDonneeUtilisateur().getExtensionTypeDonneeUtilisateur();
 					
 					if(typeAttendu != 'all'){
-						//config //typeImageAutorises //extensionsImageAutorisees
+						// On charge le fichier de configuration
+						var config = new Config();
+						var varConfig =await(config.getVar('donneeUtilisateur','typeImage','type'));
+						// On récupère les extensions autorisées
+						var extensionsImageAutorisees  = varConfig.split(',');
+
 						if(typeAttendu != 'all.image'){
+							
 							if(typeAttendu != 'all.image.without.dicom'){
+							
 								if(typeDonnee != typeAttendu){
-									console.log("return 1 false");
 									//error
+									messageClient.addErreur(tacheDemandee + 'erreur from ! all.image.without.dicom');
+									return false;								
+								}
+							}else{
+									
+									extensionsImageAutorisees.splice(extensionsImageAutorisees.indexOf('dcm'),1);
+									extensionsImageAutorisees.splice(extensionsImageAutorisees.indexOf('dcm'),1);
+									if(extensionsImageAutorisees.indexOf(typeDonnee)<0){
+										messageClient.addErreur(tacheDemandee + 'erreur from all image without dicom')
+									}														
+							}
+						}else {
+								if(extensionsImageAutorisees.indexOf(typeDonnee)<0){
+									messageClient.addErreur(tacheDemandee + 'erreur from all image');
 									return false;
 								}
-								}else{
-									// On retire l'élément 'dcm' du tableau des extensions
-
-								}
-							}else {
-								// all.image
-							}
+						}
 					}
 				});
 			} else{
-				//error
-				console.log("return 2 false");
+				messageClient.addErreur(tacheDemandee + 'no match data');
 				return false;
 			}
 			});
@@ -144,7 +158,8 @@ function executeRun(fields,currentUtilisateur, applicationRunning,numVersionRunn
 					var parametres = await(fonction.getParametres());
 					if(parametres!= false){
 						parametres.forEach(function(parametre){
-							if(fields[parametre.getIdParametre()] != undefined || parametre.getStatutPublicParametre()){
+
+							if(fields[parametre.getIdParametre()] != undefined && parametre.getStatutPublicParametre()){
 								var valueParam = fields[parametre.getIdParametre()][0];
 								if(valueParam <= parametre.getValeurMaxParametre() && valueParam >= parametre.getValeurMinParametre()){
 									params[parametre.getNomParametre()] = valueParam;
@@ -155,7 +170,7 @@ function executeRun(fields,currentUtilisateur, applicationRunning,numVersionRunn
 								params[parametre.getNomParametre()]= parametre.getValeurDefautParametre();
 							}
 						});
-				}
+					}
 					// On transforme le tableau d'arguments en chaine de caratères
 					args = args.join('§');
 					params = params.join('§');
@@ -165,12 +180,12 @@ function executeRun(fields,currentUtilisateur, applicationRunning,numVersionRunn
 			        	args= args+ '§'+ outputData;
 			        }
 			        // Execution du script Bash pour executer une fonction de l'application
-			        outputData=execFct(createur,currentUtilisateur, applicationRunning, numVersionRunning, fonction, args);
-			        	
-			       
-			        var result = outputData.toString().trim();
+			        outputData=execFct(createur,currentUtilisateur, applicationRunning, numVersionRunning, fonction, args);	
+			      	console.log("output "+outputData);
+			      	var result = outputData.toString().trim();
 			        result= result.split('<br>').join('').split('<br />').join('').split("\n").join('').split("\r" ).join('');
 			       	result=unescape(encodeURIComponent(result));
+			       	console.log(result);
 			       	try{
 			       		result= JSON.parse(result);	
 			       		 if(outputData != undefined && outputData != ""){
@@ -179,13 +194,10 @@ function executeRun(fields,currentUtilisateur, applicationRunning,numVersionRunn
 				         	outputData=false;
 				         }
 			       	}catch (e){
+			       		console.log(e);
 			       		outputData = '{"erreurs": "'+outputData+'"}';
 			      
-			       	}
-			       	
-			        
-
-			        
+			       	}  
 
 				});
 
@@ -197,38 +209,46 @@ function executeRun(fields,currentUtilisateur, applicationRunning,numVersionRunn
 
 
 execFct = function(createur, utilisateur, application, numVersion,fonction, args){
+		
 		if(createur instanceof User && utilisateur instanceof User && application instanceof Application && fonction instanceof Fonction){
-			
 			var nomUtilisateur = utilisateur.getVariableFixeUtilisateur();
 			var nomCreateur = createur.getVariableFixeUtilisateur();
 			var nomApplication = application.getVariableFixeApplication();
 			var nameFunction = strrchr(fonction.getUrlFonction(),'/').substr(1);
-
 			var instructions = '/home/noolibco/Library/ScriptsBash/Debian/LancementApplicationServeurProd '+nomCreateur+' '+nomUtilisateur+' '+nomApplication+' '+numVersion+' '+nameFunction+' '+args;
-			 return exec(instructions);
-			
+			try{
+				var test= exec(instructions+' 2>&1');
+				console.log(instructions);
+				return test;		
+			}catch(e){
+				console.log("stat "+e.status);
+				console.log("msg "+e.message);
+				console.log("sterr "+e.stderr);
+			}
+				
 		}else{
-			console.log("erreur execute Shell")
+			return "erreur exec fct";
 		}
-	}
+			
+}	
 
 delFolderInProd = function (utilisateur){
-		if(utilisateur instanceof User){
+	
+	 	if(utilisateur instanceof User){
 			var instructions = '/home/noolibco/Library/ScriptsBash/Debian/SuppressionUtilisateurInProd '+utilisateur.getVariableFixeUtilisateur();
-			exec(instructions);
+			return exec(instructions);
 		}else{
-			console.log("error delFolderInProd");
+			return "error delFolderInProd";
 		}
 }
 //********Request********
 router.post('/', function(req, res) { 
-	res.header("Access-Control-Allow-Origin","http://172.16.72.47");
+	res.header("Access-Control-Allow-Origin","http://172.16.64.3");
 	async (function(){
 
 		var fields = await(getFormData(req));
 		var currentUtilisateur=await(user.getUtilisateurById(fields['id'][0]));
-	var currentApplication=await(application.getApplicationByIdWithAllParameters(fields['idApplication'][0]));
-
+		var currentApplication=await(application.getApplicationByIdWithAllParameters(fields['idApplication'][0]));
 	
 		if(currentApplication != false){
 
@@ -358,11 +378,48 @@ router.post('/', function(req, res) {
 		 				tacheDemandee = false;
 		 				++i;
 		 			}
-		 			delFolderInProd(currentUtilisateur);
+		 					delFolderInProd(currentUtilisateur);
 		 		}
 		 	}
+
+		 	var response = {};
+			if(messageClient.hasErreur()){
+				var texte = '';
+				messageClient.getErreurs().foreach(function(erreur){
+					texte += '<p>'+erreur+'</p>';
+				});
+				console.log("here");
+				reponse['erreurs'] = texte;
+			}
+
+			if(messageClient.hasReussite()){
+				
+				var resultatsApplication = [];
+				var i=0;
+				messageClient.getReussites().forEach(function(resultat){
+					// Pour supprimer les espaces en début/fin de chaîne ainsi que les retours chariots
+					resultat = resultat.toString().trim();
+					
+					resultat = resultat.split('<br>').join('').split('<br />').join('').split("\n").join('').split("\r" ).join('');
+					// Pour supprimer tout ce qu'il y a avant et après les {} du résultat // Eviter les headers par exemple de php
+					resultat = resultat.split('/^(.*?){/').join('{'); // Avant
+					resultat = resultat.split('/(.*)}.*$/').join('$1}'); // Après
+					// Pour un encodage en UTF8
+				    resultat = unescape(encodeURIComponent(resultat));
+					resultat = resultat.split('/(\/home\/noolibco\/.+)/').join(''); // Retire les noms des chemin du serveur NooLib
+					// Pour les failles de type scripts
+					resultat = escapeHtml(resultat);
+					resultatsApplication.push(resultat);
+				});
+				response.resultat = resultatsApplication;
+			}
+		
+			response = JSON.stringify(response);
+			response = JSON.parse(response);
+			console.log(response);
+		 	res.send(response);	
 		 	
-		 	res.send(outputData);
+		 	
 					
 				}
 			}
@@ -371,5 +428,9 @@ router.post('/', function(req, res) {
 
 });
 
+function escapeHtml(text){
+	return text.split('/&/g').join('&amp;').split('/</g').join('&lt;').split('/>/g').join('&gt;');
+	//.split('/"/g').join('&quot;').split("/'/g").join('&#39;')
+}
 
 module.exports = router;
