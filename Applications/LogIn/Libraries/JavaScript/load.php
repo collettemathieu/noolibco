20341eee9ef1fcbc8c67325fad849a825e8c5e6c
noolibco <noolibco@DebianNooLib>
2017-07-18 12:47:37 +0200

    Update Team LogIn

--- Applications/LogIn/Libraries/JavaScript/load.php
+++ Applications/LogIn/Libraries/JavaScript/load.php
@@ -1,6 +1,6 @@
 <!-- FrameWorks and Libraries JS -->	
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script type="text/javascript" src="/JavaScript/Frameworks/bootstrapForLogInApplication.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="/JavaScript/Frameworks/bootstrapSwitch.min.js"></script>
 
 <!-- Critical scripts -->
--- public_html/footer.php
+++ public_html/footer.php
@@ -4,6 +4,7 @@
 				<div class="row">
 					<div class="col-lg-5 centering" >
 						<h5>© 2017 NooLib - All rights reserved - </h5>
+						<button class="btn btn-primary btn-xs" data-toggle="modal" data-target="#team">Team</button>
 						<button ng-click="contactModal()" class="btn btn-default btn-xs">Contact</button>
 						<ul>
 							<li><a href="https://twitter.com/NoolibApp" target="_blank"><img src="/Images/Social/twitter.png"/></a></li>
@@ -15,7 +16,54 @@
 			</div>
 			<?php } ?>
 		</footer>
+		<?php if(!$user->isAuthenticated()){ ?>
+		<div id="team" class="modal fade" role="dialog">
+		  <div class="modal-dialog modal-lg">
 
+		    <!-- Modal content-->
+		    <div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">x</button>
+					<h2 class="teamTitle modal-title">The NooLib Team</h2>
+				</div>
+				<div class="modal-body">
+					<ul class="list-unstyled">
+						<li><h3>Authors (Thanks a lot)</h3>
+							<ul class="list-unstyled">
+								<li class="teamPicture"><span></span><p class="text-center">Léna Buron (2 months)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Corentin Chevallier (2 months)</p></li>
+								<li class="teamPicture"><a href="/Profile/idAuteur=122"><span></span><p class="text-center">Quentin Denis (2 months)</p></a></li>
+								<li class="teamPicture"><a href="/Profile/idAuteur=79"><span></span><p class="text-center">Guénaël Dequeker (5 months)</p></a></li>
+								<li class="teamPicture"><span></span><p class="text-center">Steve Despres (3 months)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Remi Dugue (2 months)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Antoine Fauchard (2 months)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Kévin Grosbois (1 month)</p></li>
+								<li class="teamPicture"><a href="/Profile/idAuteur=97"><span></span><p class="text-center">Naoures Hassine (6 months)</p></a></li>
+								<li class="teamPicture"><span></span><p class="text-center">Baptiste Houssais (1 month)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Brian Le Bras (1 month)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Jean Mainguy (1 month)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Baptiste Maudet (2 months)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Yohann Pichois (1 month)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Clément Richard (1 month)</p></li>
+							</ul>
+						</li>
+						<li><h3>Founder & author</h3><ul class="list-unstyled"><li class="teamPicture"><a href="/Profile/idAuteur=58"><div class="founder"></div><p class="text-center">Mathieu Collette</p></a></li></ul></li>
+						<li><h3>Partners</h3>
+						<ul class="list-inline">
+							<li>
+								<a href="https://www.chu-angers.fr/" target="_blank"><img src="/Images/Social/chuAngers.png" alt="Centre hospitalier et universitaire d'Angers"/></a>
+							</li>
+							<li>
+								<a href="http://www.uco.fr/ima/" target="_blank"><img src="/Images/Social/ima.jpg" alt="Institut de Mathématiques Appliquées de l'Université Catholique de l'Ouest"/></a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+		<?php } ?>
 		<?php if($user->isAuthenticated() && $user->jsIsActivated()) {
 			if((!$user->getAttribute('isAdmin') && $_SERVER['REQUEST_URI'] != '/PourAdminSeulement/') || (bool) strstr($_SERVER['REQUEST_URI'],'/NooSpace/a=')) {?>
 			<div id="overlayDockApplication">
