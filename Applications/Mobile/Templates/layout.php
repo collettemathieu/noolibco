6f57eaf325eda4b2efcf3e1ca60331f44f256de6
collettemathieu <collettemathieu@noolib.com>
2017-08-23 16:35:36 +0200

    Update Social media

--- Applications/Mobile/Templates/layout.php
+++ Applications/Mobile/Templates/layout.php
@@ -79,7 +79,7 @@ Go to https://www.noolib.com/Hire/ .
 				<div class="row">
 					<div class="col-lg-12 centering maxWidth">
 						<div class="col-lg-8 text-center centering">
-							<h5>© 2017 NooLib - All rights reserved</h5>
+							<h5>© 2017 NooLib - All rights reserved - </h5>
 							<ul>
 								<li><a href="https://www.tipeee.com/noolib" target="_blank" data-toggle="tooltip" title="Help us with tipeee!"><img src="/Images/Social/tipeee.png"/></a></li>
 								<li><a href="https://blog.noolib.com" target="_blank" data-toggle="tooltip" title="NooLib The Blog!"><img src="/Images/Social/blog.png"/></a></li>
