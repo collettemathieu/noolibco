1f4d7f97c06b6cfa901f7b3e333208ba7ab766aa
collettemathieu <collettemathieu@noolib.com>
2017-08-23 11:33:58 +0200

    Update Blog NooLib Email

--- Blog/Applications/ApplicationsStandAlone/Mail/Templates/layout.php
+++ Blog/Applications/ApplicationsStandAlone/Mail/Templates/layout.php
@@ -1,12 +1,12 @@
 <?php
 // +----------------------------------------------------------------------+
-// | PHP Version 5                                                        |
+// | PHP Version 7 								                          |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 2016 ScienceAPart									  |
+// | Copyright (c) 2017 NooLib The Blog	         				          |
 // +----------------------------------------------------------------------+
-// | Layout d'un email issu et adapté depuis CampaignMonitor.co			  |	  										  |
+// | Layout des emails													  |
 // +----------------------------------------------------------------------+
-// | Auteurs : Mathieu COLLETTE <collettemathieu@scienceapart.com> 		  |
+// | Auteur : Mathieu COLLETTE <collettemathieu@noolib.com	    		  |
 // +----------------------------------------------------------------------+
 
 
@@ -15,7 +15,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Email ScienceAPart</title>
+<title>Email NooLib The Blog</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body style="margin: 0; padding: 0;">
@@ -25,7 +25,7 @@
 				<table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border: 1px solid #cccccc; border-collapse: collapse;">
 					<tr>
 						<td align="center" bgcolor="#79c8e2" style="padding: 40px 0 30px 0; color: #153643; font-size: 28px; font-weight: bold; font-family: Arial, sans-serif;">
-							<img src="http://www.scienceapart.com/Images/Mail/h1.gif" alt="Email ScienceAPart" width="300" height="230" style="display: block;" />
+							<img src="https://blog.noolib.com/Images/Mail/h1.gif" alt="Email NooLib The Blog" width="300" height="230" style="display: block;" />
 						</td>
 					</tr>
 					<?php echo $content;?>
@@ -34,21 +34,21 @@
 							<table border="0" cellpadding="0" cellspacing="0" width="100%">
 								<tr>
 									<td style="color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;" width="75%">
-										&reg; ScienceAPart, 2016<br/>
-										<a href="http://www.scienceapart.com/NewsLetter/Desinscription/mail=<?php echo $destinataires;?>" style="color: #ffffff;"><font color="#ffffff">Désinscription</font></a> à la newsletter
+										&reg; NooLib, 2017<br/>
+										<a href="https://blog.noolib.com/NewsLetter/Desinscription/mail=<?php echo $destinataires;?>" style="color: #ffffff;"><font color="#ffffff">Désinscription</font></a> à la newsletter
 									</td>
 									<td align="right" width="25%">
 										<table border="0" cellpadding="0" cellspacing="0">
 											<tr>
 												<td style="font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;">
-													<a href="https://twitter.com/Science_A_Part" style="color: #ffffff;">
-														<img src="http://www.scienceapart.com/Images/Mail/tw.gif" alt="Twitter" width="38" height="38" style="display: block;" border="0" />
+													<a href="https://twitter.com/NoolibApp" style="color: #ffffff;">
+														<img src="https://blog.noolib.com/Images/Mail/tw.gif" alt="Twitter" width="38" height="38" style="display: block;" border="0" />
 													</a>
 												</td>
 												<td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
 												<td style="font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;">
-													<a href="https://www.facebook.com/scienceapart/" style="color: #ffffff;">
-														<img src="http://www.scienceapart.com/Images/Mail/fb.gif" alt="Facebook" width="38" height="38" style="display: block;" border="0" />
+													<a href="https://www.facebook.com/Noolib-1648329638748950/" style="color: #ffffff;">
+														<img src="https://blog.noolib.com/Images/Mail/fb.gif" alt="Facebook" width="38" height="38" style="display: block;" border="0" />
 													</a>
 												</td>
 											</tr>
