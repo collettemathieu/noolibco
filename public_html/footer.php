0e9e200bcf3b0d38506d27ec5e1f2bada67e45b0
collettemathieu <collettemathieu@noolib.com>
2017-08-24 16:44:43 +0200

    Button LogIn

--- public_html/footer.php
+++ public_html/footer.php
@@ -4,9 +4,9 @@
 				<div class="row">
 					<div class="col-lg-5 centering" >
 						<h5>© 2017 NooLib - All rights reserved - </h5>
-						<a href="https://blog.noolib.com" class="btn btn-default btn-xs">The Blog</a>
-						<button class="btn btn-primary btn-xs" data-toggle="modal" data-target="#team">Team</button>
-						<button ng-click="contactModal()" class="btn btn-default btn-xs">Contact</button>
+						<a href="https://blog.noolib.com" class="btn btn-xs">The Blog</a>
+						<button class="btn btn-default btn-xs" data-toggle="modal" data-target="#team">Team</button>
+						<button ng-click="contactModal()" class="btn btn-primary btn-xs">Contact</button>
 						<ul>
 							<li><a href="https://www.tipeee.com/noolib" target="_blank" data-toggle="tooltip" title="Help us on tipeee!"><img src="/Images/Social/tipeee.png"/></a></li>
 						</ul>
