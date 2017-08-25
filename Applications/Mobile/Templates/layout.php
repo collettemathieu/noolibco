335bf9e89157de85df13369c85ca2d7197c7dac0
collettemathieu <collettemathieu@noolib.com>
2017-08-25 09:27:23 +0200

    CSS Mobile Layout

--- Applications/Mobile/Templates/layout.php
+++ Applications/Mobile/Templates/layout.php
@@ -80,15 +80,64 @@ Go to https://www.noolib.com/Hire/ .
 					<div class="col-lg-12 centering maxWidth">
 						<div class="col-lg-7 text-center centering">
 							<h5>© 2017 NooLib - All rights reserved - </h5>
+							<a href="https://blog.noolib.com" target="_blank" class="btn btn-primary btn-xs" data-toggle="tooltip" title="The NooLib Blog!">The Blog</a>
+							<button class="btn btn-default btn-xs" data-toggle="modal" data-target="#team">Team</button>
+							<button ng-click="contactModal()" class="btn btn-primary btn-xs" data-toggle="tooltip" title="Contact us!">Contact</button>
 							<ul>
-								<li><a href="https://www.tipeee.com/noolib" target="_blank" data-toggle="tooltip" title="Help us with tipeee!"><img src="/Images/Social/tipeee.png"/></a></li>
-								<li><a href="https://blog.noolib.com" target="_blank" data-toggle="tooltip" title="NooLib The Blog!"><img src="/Images/Social/blog.png"/></a></li>
+								<li><a href="https://www.tipeee.com/noolib" target="_blank" data-toggle="tooltip" title="Help us on tipeee!"><img src="/Images/Social/tipeee.png"/></a></li>
 							</ul>
 						</div>
 					</div>
 				</div>
 			</div>
 		</footer>
+
+		<div id="team" class="modal fade" role="dialog">
+		  <div class="modal-dialog modal-lg">
+
+		    <!-- Modal content-->
+		    <div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">x</button>
+					<h2 class="teamTitle modal-title">The NooLib Team</h2>
+				</div>
+				<div class="modal-body">
+					<ul class="list-unstyled">
+						<li><h3>Authors (Thanks a lot)</h3>
+							<ul class="list-unstyled">
+								<li class="teamPicture"><span></span><p class="text-center">Léna Buron (2 months)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Corentin Chevallier (2 months)</p></li>
+								<li class="teamPicture"><a href="/Profile/idAuteur=122"><span></span><p class="text-center">Quentin Denis (2 months)</p></a></li>
+								<li class="teamPicture"><a href="/Profile/idAuteur=79"><span></span><p class="text-center">Guénaël Dequeker (5 months)</p></a></li>
+								<li class="teamPicture"><span></span><p class="text-center">Steve Despres (3 months)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Remi Dugue (2 months)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Antoine Fauchard (2 months)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Kévin Grosbois (1 month)</p></li>
+								<li class="teamPicture"><a href="/Profile/idAuteur=97"><span></span><p class="text-center">Naoures Hassine (6 months)</p></a></li>
+								<li class="teamPicture"><span></span><p class="text-center">Baptiste Houssais (1 month)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Brian Le Bras (1 month)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Jean Mainguy (1 month)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Baptiste Maudet (2 months)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Yohann Pichois (1 month)</p></li>
+								<li class="teamPicture"><span></span><p class="text-center">Clément Richard (1 month)</p></li>
+							</ul>
+						</li>
+						<li><h3>Founder & author</h3><ul class="list-unstyled"><li class="teamPicture"><a href="/Profile/idAuteur=58"><div class="founder"></div><p class="text-center">Mathieu Collette</p></a></li></ul></li>
+						<li><h3>Partners</h3>
+						<ul class="list-inline">
+							<li>
+								<a href="https://www.chu-angers.fr/" target="_blank"><img src="/Images/Social/chuAngers.png" alt="Centre hospitalier et universitaire d'Angers"/></a>
+							</li>
+							<li>
+								<a href="http://www.uco.fr/ima/" target="_blank"><img src="/Images/Social/ima.jpg" alt="Institut de Mathématiques Appliquées de l'Université Catholique de l'Ouest"/></a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+		      </div>
+		    </div>
+		  </div>
+		</div>
 
 		<!-- Chargement des librairies JS -->
 		<?php echo $librairiesJS; ?>
