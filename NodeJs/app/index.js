4fcfc31a9602203763a7b78744d9ff6862ab49c3
noolibco <noolibco@debianNoolib>
2017-08-28 17:20:38 +0200

    modifié :         app/index.js

--- NodeJs/app/index.js
+++ NodeJs/app/index.js
@@ -234,7 +234,7 @@ execFct = function(nbReq,createur, utilisateur, application, numVersion,fonction
 				var nomApplication = application.getVariableFixeApplication();
 				var nameFunction = strrchr(fonction.getUrlFonction(),'/').substr(1);
 				var instructions = '/home/noolibco/Library/ScriptsBash/Debian/LancementApplicationServeurProd '+nomCreateur+' '+nomUtilisateur+' '+nomApplication+' '+numVersion+' '+nameFunction+' '+nbReq+' '+args;
-				//console.log(instructions);
+				console.log(instructions);
 
 					var resultat=exec(instructions + '2>&1', {maxBuffer: 1024*50000} ,async function(err,stdout,stderr){
 						if(err)  return resolve(err);
