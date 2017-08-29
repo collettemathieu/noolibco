a4595c96fc37dd29bca210f72d0493264dd6cd73
collettemathieu <collettemathieu@noolib.com>
2017-08-29 21:24:31 +0200

    Bugs Fixed DataBox

--- Applications/NooSpace/Modules/Defaut/Views/show.php
+++ Applications/NooSpace/Modules/Defaut/Views/show.php
@@ -7,16 +7,15 @@
 				$taches = $version->getTaches();
 				?>
 					<div class="appInDock runIt" id="<?php echo $applicationNooSpace->getIdApplication();?>" idVersion="<?php echo $version->getIdVersion();?>">
-						<div class="containerApplication">
-							<div class="top">
-								<img class="imageApplication" src="data:image/png;charset=utf8;base64,<?php echo base64_encode(file_get_contents($application->getUrlLogoApplication())); ?>" alt="Logo application"/>&brvbar;
-								<div class="playButton"><img src="/Images/play.png"/></div>
-							</div>
-							<div class="ajaxLoaderApplication"><img src="/Images/waiter.gif"/></div><!--
-							--><hr><div class="resultBox">
-								<img src="/Images/results.png"/>
-								<div class="applicationReports hidden"></div>
-							</div>
+						<div class="containerApplication" style="display:inline-flex;">
+							<!--hr--><!--
+							-->
+							<div class="ajaxLoaderApplication"><img src="/Images/waiter.gif"/></div>
+							<img class="imageApplication" src="data:image/png;charset=utf8;base64,<?php echo base64_encode(file_get_contents($applicationNooSpace->getUrlLogoApplication())); ?>"/>
+							<hr><div class="resultBox">
+									<img src="/Images/results.png"/>
+									<div class="applicationReports hidden"></div>
+								</div>
 						</div>
 						<div class="tachesApplication"></div>
 					</div>
