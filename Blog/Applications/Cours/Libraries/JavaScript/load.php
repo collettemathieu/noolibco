97400244cc88198ce0a3e31142f25b25e9958baa
noolibco <noolibco@DebianNooLib>
2017-09-17 17:09:34 +0200

    Library for Blog

--- Blog/Applications/Cours/Libraries/JavaScript/load.php
+++ Blog/Applications/Cours/Libraries/JavaScript/load.php
@@ -14,3 +14,4 @@ var txlist = document.getElementsByTagName("tex");
     }
 </script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+<script type="text/javascript">hljs.initHighlightingOnLoad();</script>
