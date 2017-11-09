459dbb0c8ddf7670cd4fcbca2d2be3e03c064a60
collettemathieu <collettemathieu@noolib.com>
2017-11-09 19:46:49 +0100

    Improve JS Secured

--- Blog/Applications/Cours/Libraries/JavaScript/load.php
+++ Blog/Applications/Cours/Libraries/JavaScript/load.php
@@ -1,17 +1,4 @@
-<script type="text/javascript" src="/JavaScript/initCours.js"></script>
-<script type="text/javascript" src="/JavaScript/managerCommentaires.js"></script>
 <script type="text/javascript" src="/JavaScript/Frameworks/katex.min.js"></script>
 <script type="text/javascript" src="/JavaScript/Frameworks/katexAutoRender.min.js"></script>
-<script> 
-var txlist = document.getElementsByTagName("tex");
-    for (var i = 0; i < txlist.length; i++) {
-        var tx = txlist[i];
-        var txtext = "\\displaystyle " + tx.textContent;
-        var html = katex.renderToString(txtext, tx, { displayMode: true });
-        html = "<div class='text-center mobile'>" + html 
-                   + "<span class='pull-right'>(" + (i+1) + ")</span></div>";
-        tx.innerHTML = html;
-    }
-</script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
-<script type="text/javascript">hljs.initHighlightingOnLoad();</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+<script type="text/javascript" src="/JavaScript/initArticles.js"></script>
---
+++ Blog/public_html/JavaScript/initArticles.js
@@ -0,0 +1,13 @@
+(function(){
+    var txlist = document.getElementsByTagName("tex");
+    for (var i = 0; i < txlist.length; i++) {
+        var tx = txlist[i];
+        var txtext = "\\displaystyle " + tx.textContent;
+        var html = katex.renderToString(txtext, tx, { displayMode: true });
+        html = "<div class='text-center mobile'>" + html 
+                   + "<span class='pull-right'>(" + (i+1) + ")</span></div>";
+        tx.innerHTML = html;
+    }
+
+    hljs.initHighlightingOnLoad();
+})();
